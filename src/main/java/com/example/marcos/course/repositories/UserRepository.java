b09c480bb0ddaebada74eb31cd38e46169a737d2
Marcos Ramos <rramos@pop-os.localdomain>
2020-07-24 10:27:42 -0300

    H2 database, test profile, JPA

---
+++ src/main/java/com/example/marcos/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.marcos.course.repositories;
+
+import com.example.marcos.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
